e26652605a3f5ab769e916a1ac64007f99a9188d
Grace Kostanich <fl5812597@gmail.com>
2022-09-09 14:18:27 -0700

    WIP: Add methods to select and add tea

--- src/components/TeaControl.js
+++ src/components/TeaControl.js
@@ -8,29 +8,46 @@ class TeaControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
       formVisibleOnPage: false,
       mainTeaList: [],
       selectedTea: null
     };
   }
 
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  handleChangingSelectedTea = (id) => {
+    const newSelectedTea = this.state.mainTeaList.filter(tea => tea.id === id)[0];
+    this.setState({selectedTea: newSelectedTea});
+  }
+
+  handleAddingNewTeaToList = (newTea) => {
+    const newMainTeaList = this.state.mainTeaList.concat(newTea);
+    this.setState({mainTeaList: newMainTeaList,
+                  formVisibleOnPage: false });
+  }
+
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.selectedTea != null) {
-      currentlyVisibleState = <TeaDetail />
+      currentlyVisibleState = <TeaDetail tea = {this.state.selectedTea}/>
       buttonText = "Return to Tea List";
     } else if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTeaForm />;
+      currentlyVisibleState = <NewTeaForm onNewTeaCreation={this.handleAddingNewTeaToList}/>;
       buttonText = "Return to Tea List";
     } else {
-      currentlyVisibleState = <TeaList />;
+      currentlyVisibleState = <TeaList teaList = {this.mainTeaList} onTeaSelection = {this.handleChangingSelectedTea}/>;
       buttonText = "Add Tea";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
